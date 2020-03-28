55138e07b716456526708e7c10274c5d9f300577
hshte <hshte@softserveinc.com>
2020-03-28 17:38:18 +0200

    initial commit

---
+++ src/test/java/ua/kpi/dziuba/gasstation/gstation/GasStationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.dziuba.gasstation.gstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
