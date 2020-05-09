e00202d3acb3133b6d3c4127d812c58d5236221e
KeygenGithub <KeygenGithub@users.noreply.github.com>
2020-05-10 00:08:43 +0300

    buyfuel.html user interface created overview:     -created dropdown lists     -parsed response data     -filed in dropdowns with parsed data

--- frontend/site/resourses/parsing.js
+++ frontend/site/resourses/parsing.js
@@ -1,101 +1,429 @@
-const fuels = [
+allStations = [
     {
         "id": 1,
-        "type": "Diesel",
-        "name": "EURO",
-        "price": 1.12
+        "idLocal": 1,
+        "fuelList": [
+            {
+                "id": 1,
+                "type": "Diesel",
+                "name": "EURO",
+                "price": 1.12
+            },
+            {
+                "id": 4,
+                "type": "Gas",
+                "name": "GazProm",
+                "price": 5.4
+            },
+            {
+                "id": 5,
+                "type": "Gas",
+                "name": "PropanTM",
+                "price": 5.8
+            },
+            {
+                "id": 6,
+                "type": "Gas",
+                "name": "Ukr",
+                "price": 6.1
+            },
+            {
+                "id": 14,
+                "type": "Gas",
+                "name": "GazProm",
+                "price": 5.4
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Бориспіль АЗС"
     },
     {
         "id": 2,
-        "type": "Diesel",
-        "name": "ECO",
-        "price": 1.23
+        "idLocal": 2,
+        "fuelList": [
+            {
+                "id": 2,
+                "type": "Diesel",
+                "name": "ECO",
+                "price": 1.23
+            },
+            {
+                "id": 7,
+                "type": "Petrol",
+                "name": "A-100",
+                "price": 1.35
+            },
+            {
+                "id": 8,
+                "type": "Petrol",
+                "name": "A-98",
+                "price": 1.29
+            },
+            {
+                "id": 9,
+                "type": "Petrol",
+                "name": "A-95",
+                "price": 1.2
+            },
+            {
+                "id": 12,
+                "type": "Diesel",
+                "name": "ECO",
+                "price": 1.23
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Бориспіль АЗС"
     },
     {
         "id": 3,
-        "type": "Diesel",
-        "name": "ULTRA",
-        "price": 1.3
+        "idLocal": 3,
+        "fuelList": [
+            {
+                "id": 3,
+                "type": "Diesel",
+                "name": "ULTRA",
+                "price": 1.3
+            },
+            {
+                "id": 10,
+                "type": "Petrol",
+                "name": "A-92",
+                "price": 1.15
+            },
+            {
+                "id": 11,
+                "type": "Petrol",
+                "name": "A-88",
+                "price": 1.11
+            },
+            {
+                "id": 13,
+                "type": "Diesel",
+                "name": "ULTRA",
+                "price": 1.3
+            },
+            {
+                "id": 37,
+                "type": "Petrol",
+                "name": "A-100",
+                "price": 1.35
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Бориспіль АЗС"
     },
     {
         "id": 4,
-        "type": "Gas",
-        "name": "GazProm",
-        "price": 5.4
+        "idLocal": 4,
+        "fuelList": [
+            {
+                "id": 15,
+                "type": "Gas",
+                "name": "PropanTM",
+                "price": 5.8
+            },
+            {
+                "id": 16,
+                "type": "Gas",
+                "name": "Ukr",
+                "price": 6.1
+            },
+            {
+                "id": 28,
+                "type": "Petrol",
+                "name": "A-98",
+                "price": 1.29
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Бориспіль АЗС"
     },
     {
         "id": 5,
-        "type": "Gas",
-        "name": "PropanTM",
-        "price": 5.8
+        "idLocal": 5,
+        "fuelList": [
+            {
+                "id": 19,
+                "type": "Petrol",
+                "name": "A-95",
+                "price": 1.2
+            },
+            {
+                "id": 20,
+                "type": "Petrol",
+                "name": "A-92",
+                "price": 1.15
+            },
+            {
+                "id": 21,
+                "type": "Petrol",
+                "name": "A-88",
+                "price": 1.11
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Бориспіль АЗС"
     },
     {
         "id": 6,
-        "type": "Gas",
-        "name": "Ukr",
-        "price": 6.1
+        "idLocal": 1,
+        "fuelList": [
+            {
+                "id": 17,
+                "type": "Petrol",
+                "name": "A-100",
+                "price": 1.35
+            },
+            {
+                "id": 18,
+                "type": "Petrol",
+                "name": "A-98",
+                "price": 1.29
+            },
+            {
+                "id": 29,
+                "type": "Petrol",
+                "name": "A-95",
+                "price": 1.2
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
     },
     {
         "id": 7,
-        "type": "Petrol",
-        "name": "A-100",
-        "price": 1.35
+        "idLocal": 2,
+        "fuelList": [
+            {
+                "id": 22,
+                "type": "Diesel",
+                "name": "ECO",
+                "price": 1.23
+            },
+            {
+                "id": 27,
+                "type": "Petrol",
+                "name": "A-100",
+                "price": 1.35
+            },
+            {
+                "id": 39,
+                "type": "Petrol",
+                "name": "A-95",
+                "price": 1.2
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
     },
     {
         "id": 8,
-        "type": "Petrol",
-        "name": "A-98",
-        "price": 1.29
+        "idLocal": 3,
+        "fuelList": [
+            {
+                "id": 23,
+                "type": "Diesel",
+                "name": "ULTRA",
+                "price": 1.3
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
     },
     {
         "id": 9,
-        "type": "Petrol",
-        "name": "A-95",
-        "price": 1.2
+        "idLocal": 4,
+        "fuelList": [
+            {
+                "id": 24,
+                "type": "Gas",
+                "name": "GazProm",
+                "price": 5.4
+            },
+            {
+                "id": 25,
+                "type": "Gas",
+                "name": "PropanTM",
+                "price": 5.8
+            },
+            {
+                "id": 38,
+                "type": "Petrol",
+                "name": "A-98",
+                "price": 1.29
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
     },
     {
         "id": 10,
-        "type": "Petrol",
-        "name": "A-92",
-        "price": 1.15
+        "idLocal": 5,
+        "fuelList": [
+            {
+                "id": 26,
+                "type": "Gas",
+                "name": "Ukr",
+                "price": 6.1
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
     },
     {
         "id": 11,
-        "type": "Petrol",
-        "name": "A-88",
-        "price": 1.11
+        "idLocal": 6,
+        "fuelList": [
+            {
+                "id": 30,
+                "type": "Petrol",
+                "name": "A-92",
+                "price": 1.15
+            },
+            {
+                "id": 34,
+                "type": "Gas",
+                "name": "GazProm",
+                "price": 5.4
+            }
+        ],
+        "city": "Київ",
+        "nameStation": "Перемоги АЗС"
+    },
+    {
+        "id": 12,
+        "idLocal": 1,
+        "fuelList": [
+            {
+                "id": 31,
+                "type": "Petrol",
+                "name": "A-88",
+                "price": 1.11
+            },
+            {
+                "id": 32,
+                "type": "Diesel",
+                "name": "ECO",
+                "price": 1.23
+            }
+        ],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
+    },
+    {
+        "id": 13,
+        "idLocal": 2,
+        "fuelList": [
+            {
+                "id": 36,
+                "type": "Gas",
+                "name": "Ukr",
+                "price": 6.1
+            },
+            {
+                "id": 40,
+                "type": "Petrol",
+                "name": "A-92",
+                "price": 1.15
+            },
+            {
+                "id": 41,
+                "type": "Petrol",
+                "name": "A-88",
+                "price": 1.11
+            }
+        ],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
+    },
+    {
+        "id": 14,
+        "idLocal": 3,
+        "fuelList": [],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
+    },
+    {
+        "id": 15,
+        "idLocal": 4,
+        "fuelList": [
+            {
+                "id": 33,
+                "type": "Diesel",
+                "name": "ULTRA",
+                "price": 1.3
+            }
+        ],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
+    },
+    {
+        "id": 16,
+        "idLocal": 5,
+        "fuelList": [
+            {
+                "id": 35,
+                "type": "Gas",
+                "name": "PropanTM",
+                "price": 5.8
+            }
+        ],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
+    },
+    {
+        "id": 17,
+        "idLocal": 6,
+        "fuelList": [],
+        "city": "Тернопіль",
+        "nameStation": "Українки АЗС"
     }
 ]
 
-const fuelHashTable = {};
+const pumpHashTable = {};
 
 function init() {
 
-    fuels.forEach(fuel => {
+    allStations.forEach(element => {
+        pumpHashTable[element.city] = {};
+    });
+
+    allStations.forEach(element => {
+        const city = element.city;
+        const newStation = element.nameStation;
+
+        pumpHashTable[city][newStation] = {};
+    });
+
+    allStations.forEach(element => {
+        const city = element.city;
+        const station = element.nameStation;
+        const newIdLocal = element.idLocal;
+
+        pumpHashTable[city][station][newIdLocal] = initFuels(element);
+    });
+
+    changeName();
+}
+
+function initFuels(pump) {
+
+    const fuelHashTable = {};
+
+    pump.fuelList.forEach(fuel => {
         fuelHashTable[fuel.type] = [];
     });
 
-    fuels.forEach(fuel => {
+
+    pump.fuelList.forEach(fuel => {
         fuelHashTable[fuel.type].push({
             name: fuel.name,
             price: fuel.price
         });
     });
-}
 
-function parseData() {
-
-    let fuelTypeSelect = document.getElementById("fuel-type");
-    // let fuelNameSelect = document.getElementById("fuel-name");
-
-    Object.keys(fuelHashTable).forEach(key => {
-
-        createNewTag("option", key, fuelTypeSelect);
-    });
-
-    changeName();
-
-
+    return fuelHashTable;
 }
 
 window.onload = function () {
@@ -104,6 +432,13 @@ window.onload = function () {
     parseData();
 
 };
+
+const CITY = "city";
+const NUMBER = "number";
+const AZS = "azs";
+const FUEL_TYPE = "fuel-type";
+const FUEL_NAME = "fuel-name";
+const FUEL_PRICE = "fuel-price";
 
 function createNewTag(tag, tagTextContent, parentNode) {
 
@@ -116,68 +451,129 @@ function createNewTag(tag, tagTextContent, parentNode) {
 
 }
 
+function parseData() {
+
+    let fuelCitySelect = document.getElementById(CITY);
+    // let fuelNameSelect = document.getElementById("fuel-name");
+
+    Object.keys(pumpHashTable).forEach(key => {
+
+        createNewTag("option", key, fuelCitySelect);
+    });
+
+    changeCity();
+}
+
+function changeCity() {
+
+    cleanElement([AZS, NUMBER, FUEL_TYPE, FUEL_NAME]);
+
+    const dropdownValuesHashTable = getDropdownValuesHashTable([CITY]);
+
+    const city = dropdownValuesHashTable[CITY];
+
+    const fuelAzsSelect = document.getElementById(AZS);
+
+    Object.keys(pumpHashTable[city]).forEach(key => {
+        createNewTag("option", key, fuelAzsSelect);
+    })
+
+    changeAzs()
+}
+
+function changeAzs() {
+
+    cleanElement([NUMBER, FUEL_TYPE, FUEL_NAME]);
+
+    const dropdownValuesHashTable = getDropdownValuesHashTable([CITY, AZS]);
+
+    const city = dropdownValuesHashTable[CITY];
+    const station = dropdownValuesHashTable[AZS];
+
+    const fuelNumberSelect = document.getElementById(NUMBER);
+
+    Object.keys(pumpHashTable[city][station]).forEach(key => {
+        createNewTag("option", key, fuelNumberSelect);
+    })
+
+    changeNumber()
+}
+
+function changeNumber() {
+
+    cleanElement([FUEL_TYPE, FUEL_NAME]);
+
+    const dropdownValuesHashTable = getDropdownValuesHashTable([CITY, AZS, NUMBER]);
+
+    const city = dropdownValuesHashTable[CITY];
+    const station = dropdownValuesHashTable[AZS];
+    const number = dropdownValuesHashTable[NUMBER];
+
+    const fuelTypeSelect = document.getElementById(FUEL_TYPE);
+
+    Object.keys(pumpHashTable[city][station][number]).forEach(key => {
+        createNewTag("option", key, fuelTypeSelect);
+    })
+
+    changeName()
+}
+
+
 function changeName() {
-    const selectedIndex = document.getElementById('fuel-type').selectedIndex;
-    const options = document.getElementById('fuel-type').options;
-    const value = options[selectedIndex].value;
 
-    document.getElementById("fuel-value").innerHTML = value;
+    cleanElement([FUEL_NAME]);
 
-    clean("fuel-name");
+    const dropdownValuesHashTable = getDropdownValuesHashTable([CITY, AZS, NUMBER, FUEL_TYPE]);
 
-    getNamesByType(value);
-    changePrice();
+    const city = dropdownValuesHashTable[CITY];
+    const station = dropdownValuesHashTable[AZS];
+    const number = dropdownValuesHashTable[NUMBER];
+    const type = dropdownValuesHashTable[FUEL_TYPE];
+
+    const fuelTypeSelect = document.getElementById(FUEL_NAME);
+
+    pumpHashTable[city][station][number][type].forEach(key => {
+        createNewTag("option", key.name, fuelTypeSelect);
+    })
+
+    changePrice()
 }
 
 function changePrice() {
-    const selectedIndexType = document.getElementById('fuel-type').selectedIndex;
-    const options = document.getElementById('fuel-type').options;
-    const value = options[selectedIndexType].value;
 
-    const fuelNameSelect = document.getElementById("fuel-name");
-    const selectedIndex = fuelNameSelect.selectedIndex;
-    const nameOptions = document.getElementById('fuel-name').options;
-    const nameOptionValue = nameOptions[selectedIndex].value;
+    const dropdownValuesHashTable = getDropdownValuesHashTable([CITY, AZS, NUMBER, FUEL_TYPE, FUEL_NAME]);
 
-    getPriceByTypeAndName(value, nameOptionValue);
-}
+    const city = dropdownValuesHashTable[CITY];
+    const station = dropdownValuesHashTable[AZS];
+    const number = dropdownValuesHashTable[NUMBER];
+    const type = dropdownValuesHashTable[FUEL_TYPE];
+    const name = dropdownValuesHashTable[FUEL_NAME];
 
-function clean(nodeId) {
-
-    const select = document.getElementById(nodeId);
-    const length = select.options.length;
-
-    for (let i = length - 1; i >= 0; i--) {
-        select.options[i] = null;
-    }
-
-}
-
-function getNamesByType(type) {
-    const fuelNameSelect = document.getElementById("fuel-name");
-
-    fuelHashTable[type].forEach(fuel => {
-        createNewTag("option", fuel.name, fuelNameSelect);
-    });
-}
-
-function getPriceByTypeAndName(type, name) {
-    let fuelPriceSelect = document.getElementById("fuel-price");
-    fuelHashTable[type].forEach(fuel => {
-        if (name === fuel.name) {
-            fuelPriceSelect.innerText = fuel.price;
+    const typeList = pumpHashTable[city][station][number][type];
+    typeList.forEach(key => {
+        if(key.name === name ){
+             document.getElementById(FUEL_PRICE).innerHTML = key.price;
         }
     });
 
 }
 
+function getDropdownValuesHashTable(option) {
+    const dropdownValuesHashTable = {};
+
+    option.forEach(currentElement => {
+            const typeSelectedIndex = document.getElementById(currentElement).selectedIndex;
+            const typeOptions = document.getElementById(currentElement).options;
+            dropdownValuesHashTable[currentElement] = typeOptions[typeSelectedIndex].value;
+        }
+    );
+
+    return dropdownValuesHashTable;
+}
+
 function buyFuelbySize() {
 
-    //clean options
-    const cleanFuelPrize = document.getElementById("end-your-fuel-price");
-    cleanFuelPrize.innerHTML = '<div> </div>';
-    const cleanBuy = document.getElementById("submit");
-    cleanBuy.innerHTML = '<div> </div>';
+    cleanObject();
 
     const buyForPrize = document.getElementById("buyforprice");
     buyForPrize.innerHTML = '<button type="button" onclick="buyFuelbyPrice()" class="btn btn-info" style="width: 100%">Придбати пальне за ціною </button>';
@@ -191,11 +587,7 @@ function buyFuelbySize() {
 
 function buyFuelbyPrice() {
 
-    //clean options
-    const cleanFuelPrize = document.getElementById("end-your-fuel-price");
-    cleanFuelPrize.innerHTML = '<div> </div>';
-    const cleanBuy = document.getElementById("submit");
-    cleanBuy.innerHTML = '<div> </div>';
+    cleanObject();
 
     const buyForSize = document.getElementById("buyforsize");
     buyForSize.innerHTML = '<button type="button" onclick="buyFuelbySize()" class="btn btn-primary " style="width: 100%">Придбати пальне за об\'ємом </button>';
@@ -213,7 +605,9 @@ function countMoney() {
     const currentPrice = fuelPrice.innerHTML;
 
 
-    const endPrice = inputPrice * currentPrice;
+    let endPrice = inputPrice * currentPrice;
+    endPrice = endPrice.toFixed(2); // строковое представление результата
+
     const yourFuelPrize = document.getElementById("end-your-fuel-price");
     yourFuelPrize.innerHTML = '<div> Ваша ціна пального з вибраними опціями становить <span id="your-fuel-price"></span> грн.</div>';
     document.getElementById("your-fuel-price").innerHTML = endPrice;
@@ -229,12 +623,30 @@ function countSize() {
     const fuelPrice = document.getElementById("fuel-price");
     const currentPrice = fuelPrice.innerHTML;
 
-    const endPrice = inputPrice * currentPrice;
+    let endPrice = inputPrice / currentPrice;
+    endPrice = Math.round(endPrice);
+
     const yourFuelPrize = document.getElementById("end-your-fuel-price");
-    yourFuelPrize.innerHTML = '<div>Ви можете придбати <span id="your-fuel-price"></span>л пального за вибраними опціями</div>';
+    yourFuelPrize.innerHTML = '<div>Ви можете придбати <span id="your-fuel-price"></span>л пального за вибраними опціями.</div>';
     document.getElementById("your-fuel-price").innerHTML = endPrice;
 
     const submitBuy = document.getElementById("submit");
     submitBuy.innerHTML = '<button type="button" onclick="alert()" class="btn btn-success">Підвердити покупку</button>';
 
+}
+
+function cleanObject() {
+    const cleanFuelPrize = document.getElementById("end-your-fuel-price");
+    cleanFuelPrize.innerHTML = '<div> </div>';
+    const cleanBuy = document.getElementById("submit");
+    cleanBuy.innerHTML = '<div> </div>';
+
+}
+
+function cleanElement(needClearArr) {
+    needClearArr.forEach(elementId => {
+            const cleanFuelPrize = document.getElementById(elementId);
+            cleanFuelPrize.innerHTML = '<div> </div>';
+        }
+    );
 }
